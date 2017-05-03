7ffc5599566e748b439b76fc18ab07320c7322f6
Cathy Zhu <cathyxz@gmail.com>
2017-05-02 22:05:08 -0700

    add sample jooq code generation

--- stockviewer-server/src/main/java/com/cathyxz/stockviewer/StockViewerHealthCheck.java
+++ stockviewer-server/src/main/java/com/cathyxz/stockviewer/StockViewerHealthCheck.java
@@ -2,9 +2,6 @@ package com.cathyxz.stockviewer;
 
 import com.codahale.metrics.health.HealthCheck;
 
-/**
- * Created by cathyzhu on 4/27/17.
- */
 public class StockViewerHealthCheck extends HealthCheck {
 
     public StockViewerHealthCheck() {
